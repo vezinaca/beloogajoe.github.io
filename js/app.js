818cde31ba476c65377ed769ed101aa2388adbe1
Carl <vezinaca@gmail.com>
2020-10-30 21:29:55 -0400

    testing adding of li element

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 console.log('allo');
+
+const beloogajoe = document.querySelector('h1');
+
+beloogajoe.addEventListener('click', testCarl);
+
+
+function testCarl(){
+    console.log('allo carl');
+    const li = document.createElement('li');
+    li.textContent = 'allo';
+    document.querySelector('ul').appendChild(li);
+
+}
+
